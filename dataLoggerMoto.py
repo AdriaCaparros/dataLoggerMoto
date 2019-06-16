984826087dc1bcf504742613d322ba9611699884
adria <adria.caparros@estudiants.urv.cat>
2019-06-16 11:54:43 +0200

    update datalogger

--- dataLoggerMoto.py
+++ dataLoggerMoto.py
@@ -1,6 +1,8 @@
-mport can
+
 import time
 import os
+import can
+import math
 
 
 VEHICLE_SPEED = 0x80
@@ -29,41 +31,94 @@ bus = can.interface.Bus(channel='can0',bustype='socketcan_native')
 tpdo_1 = can.Message(data=[0, 0, 0, 0, 0, 0, 0, 0], arbitration_id=0x274)
 tpdo_2 = can.Message(data=[0, 0, 0, 0, 0, 0, 0, 0], arbitration_id=0x195)
 tpdo_3 = can.Message(data=[0, 0, 0, 0, 0, 0, 0, 0], arbitration_id=0x146)
+arduino_1 = can.Message(data=[0, 0, 0, 0, 0, 0, 0, 0], arbitration_id=0x2F)
+arduino_2 = can.Message(data=[0, 0, 0, 0, 0, 0, 0, 0], arbitration_id=0x3F)
 
-speed=0
+
 voltage=0
+corrent=0
+corrent_maxim=0
+temperatura_motor=0
+temperatura_inversor=0
+velocitat=0
+revolucions=0
+accelerador=0
 temp1=0
 c=''
 count=0
+valor=0
+
 
 
 while True:
-    for j in range(3):
+    for j in range(4):
         message = bus.recv(timeout=1.0)
         if message is not None:
-            s=0
-            for i in range(message.dlc):
-                s|=message.data[i]<<(8*i)
-                
             c='{0:f},{1:d},'.format(message.timestamp,count)
+            valor=0
             if (message.arbitration_id == 0x274):
-                print("Data: ",s)
                 print("Id: ", message.arbitration_id)
                 tpdo_1.data = message.data
-                
-            if (message.arbitration_id == 0x195):
-                print("Data: ",s)
+                # TPDO1 ---------------------------------------
+                # VOLTATGE BATERIA (V)
+                valor = tpdo_1.data[1]
+                valor = (valor << 8) | tpdo_1.data[0]  # Traiem el valor del missatge
+                voltatge = valor * 0.0625  # Apliquem el factor de multiplicació
+                print("Voltatge: ",voltatge)
+                # CORRENT BATERIA (A)
+                valor = tpdo_1.data[3]
+                valor = (valor << 8) | tpdo_1.data[2]  # Traiem el valor del missatge
+                corrent = valor * 0.0625  # Apliquem el factor de multiplicació
+                print("Corrent: ", corrent)
+                # CORRENT MAXIM DESCARREGA BATERIA (A)
+                valor = tpdo_1.data[5]
+                corrent_maxim = (valor << 8) | tpdo_1.data[4]  # Traiem el valor del missatge
+                print("Corren maxim: ", corrent_maxim)
+            if (message.arbitration_id== 0x195):
                 print("Id: ", message.arbitration_id)
                 tpdo_2.data = message.data
-                
-            if (message.arbitration_id == 0x146):
-                print("Data: ",s)
+                # TPDO2 ---------------------------------------
+                # TEMPERATURA MOTOR (ºC)
+                valor = tpdo_2.data[1]
+                temperatura_motor = (valor << 8) | tpdo_2.data[0]  # Traiem el valor del missatge
+                print("Temperatura motor: ", temperatura_motor)
+                # TEMPERATURA INVERSOR (ºC)
+                temperatura_inversor = tpdo_2.data[2]  # Traiem el valor del missatge
+                print("Temperatura inversor: ", temperatura_inversor)
+            if (message.arbitration_id==0x146):
                 print("Id: ", message.arbitration_id)
                 tpdo_3.data = message.data
-        
-    
-    c+='{0:d},{1:d},{2:d}'.format(0,0,0)
-    print('\r {} '.fromat(c))
+                # TPDO3 ---------------------------------------
+                # VELOCITAT DEL VEHICLE (kph)
+                valor = tpdo_3.data[1]
+                valor = (valor << 8) | tpdo_3.data[0]  # Traiem el valor del missatge
+                velocitat = valor * 0.0625  # Apliquem el factor de multiplicació
+                print("Velocitat: ", valor)
+                # REVOLUCIONS DEL MOTOR (rpm)
+                valor = tpdo_3.data[5]
+                valor = (valor << 8) | tpdo_3.data[4]  # Traiem el valor del missatge
+                valor = (valor << 8) | tpdo_3.data[3]  # Traiem el valor del missatge
+                valor = (valor << 8) | tpdo_3.data[2]  # Traiem el valor del missatge
+                if valor <= 10000:
+                    revolucions = valor
+                    print("RPM: ",revolucions)
+                # VOLTATGE ACCELERADOR (V)
+                valor = tpdo_3.data[7]
+                valor = (valor << 8) | tpdo_3.data[6]  # Traiem el valor del missatge
+                accelerador = valor * 0.0039  # Apliquem el factor de multiplicació
+                print("Accelerador: ", accelerador)
+
+            if(message.arbitration_id==0x2F):
+                print("Id: ", message.arbitration_id)
+                arduino_1.data = message.data
+                valor=arduino_1.data
+                print("Valor arduino 1: ",valor)
+
+
+
+
+    c+='{0:d},{1:d},{2:d},{3:d},{4:d},{5:d},{6:d},{7:d}'.format(voltatge,corrent,corrent_maxim,temperatura_motor,temperatura_inversor,velocitat,revolucions,accelerador)
+    print('\r {} '.format(c))
     print(c,file=outfile)
     count+=1
     
